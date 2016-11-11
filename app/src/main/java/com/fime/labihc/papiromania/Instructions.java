00f50ca3cfa4212238930c4ec5aa955425eda04f
Enrique D. Amaya <enriquedfa@gmail.com>
2016-11-11 15:43:17 -0600

    Pantalla inicial e instrucciones completas con tema y la madre.

--- app/src/main/java/com/fime/labihc/papiromania/Instructions.java
+++ app/src/main/java/com/fime/labihc/papiromania/Instructions.java
@@ -1,5 +1,6 @@
 package com.fime.labihc.papiromania;
 
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,5 @@ public class Instructions extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_instructions);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
